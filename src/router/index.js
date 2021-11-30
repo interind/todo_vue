568714f3f13aadf649403cee355508679bb9c587
Aleksandr <interind@yandex.ru>
2021-11-30 21:16:39 +0300

    Login

--- src/router/index.js
+++ src/router/index.js
@@ -5,19 +5,23 @@ import '../styles/index.css';
 
 Vue.use(VueRouter);
 
+const auth = '1'; // константа авторизации
+
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: Home,
+    meta: { auth: true },
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/login',
+    name: 'Login',
+    meta: { layout: 'main' },
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    component: () => import(/* webpackChunkName: "about" */ '../views/Login.vue'),
   },
 ];
 
@@ -25,6 +29,15 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  const login = to.matched.some(({ meta }) => meta.auth);
+  if (login && !auth) {
+    next('/login');
+  } else {
+    next();
+  }
 });
 
 export default router;
---
+++ src/views/Login.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="login">
+    <h1>Login in</h1>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
